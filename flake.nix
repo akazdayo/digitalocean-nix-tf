9e082aa8077a49eb5ae87d8f66c35246c848fbde
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-02-19 23:51:20 +0900

    descriptionが古かった

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Minecraft server deployment with NixOS, Terraform, and deploy-rs";
+  description = "Deployment with NixOS, Terraform, and deploy-rs";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
